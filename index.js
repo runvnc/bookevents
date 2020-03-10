e4ca2e10ccde28f9a215c55634499ffa2990a824
Jason Livesay <runvnc@gmail.com>
2020-03-10 19:19:30 +0000

    working

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fetch = require('node-fetch')
 const pgp_ = require('pg-promise')
 const pgp = pgp_()
 const readFile = require('fs').readFileSync
+const delay = require('delay')
 const AWS = require ('aws-sdk')
+const mustache = require('mustache').render
 
 const pw = process.env.pw
 
@@ -12,13 +14,13 @@ const cn = { host: 'pricesdb.cerqhtf4ui7b.us-east-2.rds.amazonaws.com',
 
 const db = pgp(cn)
 
+let lambda = null;
+
 async function initDB() {
   const initSQL = readFile(__dirname+'/init.sql','utf8')
   console.log(initSQL)
   await db.any(initSQL)
 }
-
-
 
 /* 
 runs once every minute
@@ -37,57 +39,73 @@ for each coin:
 let lambda;
 */
 
+let coins = [
+ { ourname: 'btc_usd', exchanges: { kraken: 'XXBTZUSD' } }
+]
+
+let mktparams = {
+  kraken: {
+    "url": "https://api.kraken.com/0/public/Depth?pair={{sym}}",
+    "parser": "simple",
+    "field": "result"
+  }
+}
+
+
 async function init() {
   AWS.config.region = "us-east-2"
   lambda = new AWS.Lambda()
 
 }
     
-async function bookEvent({coin, mkt}) {
-  let obj = {
-     exchange: mkt,
-     symbol: coin.ourname
-   }
+async function bookEvent({oursym, mktname, symbol, params}) {
+  let obj = { exchange: mktname, symbol: symbol+'$'+oursym}
   Object.assign(obj, params)
-
+  obj.url = mustache(obj.url, {sym:symbol})
   let lambdaArgs = {
     ClientContext: "Screener", 
     FunctionName: "getbooks", 
     InvocationType: "Event", 
     LogType: "Tail", 
-    Payload: JSON.stringify(obj), 
-    Qualifier: "1"
+    Payload: JSON.stringify(obj) //, 
+    //Qualifier: "1"
  }
+ console.log({lambdaArgs})
+
  return lambda.invoke(lambdaArgs).promise()
 
 }
 
-async function recordbooks(coins) {
-  for (coin of coins) {  
+async function recordbooks({coins, mktparams}) {
+  for (let coin of coins) {  
     try {
-      for (mkt of coin.exchanges) {
-        let toTrigger = []
+      for (let mktname of Object.keys(coin.exchanges)) {
+        const symbol = coin.exchanges[mktname]
+        const params = mktparams[mktname]
+        const oursym = coin.ourname
+        const toTrigger = []
         try {
-          toTrigger.push(bookEvent({coin, mkt}))
+          toTrigger.push(bookEvent({oursym, symbol, mktname, params}))
           await Promise.all(toTrigger)
         } catch (e) {
-          console.error("bookEvent trigger error: "+mkt + " " +e.message)
+          console.error("bookEvent trigger error: "+mktname + " " +e.message)
         } 
       }
 
     } catch (e) {
-      console.error("recordbooks error coin="+coin.name + e.message)
+      console.error("recordbooks error coin="+coin.ourname + e.message)
     }
     await delay(2000);
   }
 }
 
 exports.handler = async (event) => {
-    //let text = await getBooks(event)
     let text = 'ok'
-    //await initDB()
     await init()
-    //text = await dbquery()
+ 
+    await recordbooks({coins, mktparams}) 
+   //await initDB()
+   //text = await dbquery()
     const response = {
         statusCode: 200,
         body: text,
