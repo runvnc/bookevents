3f598aed9bbddbdd99390f666a4628dbeb47d012
Jason Livesay <runvnc@gmail.com>
2020-03-11 04:42:28 +0000

    working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const readFile = require('fs').readFileSync
 const delay = require('delay')
 const AWS = require ('aws-sdk')
 const mustache = require('mustache').render
+const groupBy = require('group-by')
 
 const pw = process.env.pw
 
@@ -39,8 +40,10 @@ for each coin:
 let lambda;
 */
 
+/*
 let coins = [
- { ourname: 'btc_usd', exchanges: { kraken: 'XXBTZUSD' } }
+ { ourname: 'btc_usd', exchanges: { kraken: 'XXBTZUSD' } },
+ { ourname: 'ltc_usd', exchanges: { kraken: 'XLTCZUSD' } }
 ]
 
 let mktparams = {
@@ -49,6 +52,21 @@ let mktparams = {
     "parser": "simple",
     "field": "result"
   }
+} */
+
+async function getparams() {
+  let params = await db.any('select lower(exchange) as exchange, params from bookparams')
+  params = groupBy(params,'exchange') 
+  let ret = {}
+  for (let [key,val] of Object.entries(params)) {
+     ret[key] = val[0].params;
+  }
+  return ret;
+}
+
+async function getpairs() {
+  let pairs = await db.any('select ourpair as ourname, exchange, theirpair from exchangepairs order by ourpair ')
+  return groupBy(pairs,'ourname')
 }
 
 
@@ -72,10 +90,35 @@ async function bookEvent({oursym, mktname, symbol, params}) {
  }
  console.log({lambdaArgs})
 
- return lambda.invoke(lambdaArgs).promise()
+// return lambda.invoke(lambdaArgs).promise()
 
 }
 
+async function recordbooks({coins, mktparams}) {
+  for (let coin of Object.keys(coins)) {  
+    try {
+      for (let mkt of coins[coin]) {
+        const symbol = mkt.theirpair
+        const params = mktparams[mkt.exchange.toLowerCase()]
+        console.log({mktparams, mkt,params})
+        const oursym = coin.ourname
+        const toTrigger = []
+        try {
+          toTrigger.push(bookEvent({oursym, symbol, mktname:mkt.exchange, params}))
+          await Promise.all(toTrigger)
+        } catch (e) {
+          console.error("bookEvent trigger error: "+mkt.exchange + " " +e.message)
+        } 
+      }
+
+    } catch (e) {
+      console.error("recordbooks error coin="+coin + e.message)
+    }
+    await delay(2000);
+  }
+}
+
+/*
 async function recordbooks({coins, mktparams}) {
   for (let coin of coins) {  
     try {
@@ -97,16 +140,18 @@ async function recordbooks({coins, mktparams}) {
     }
     await delay(2000);
   }
-}
+} */
 
 exports.handler = async (event) => {
     let text = 'ok'
     await init()
- 
-    await recordbooks({coins, mktparams}) 
+   const mktparams = await getparams()
+   const coins = await getpairs()
+   console.log({mktparams,coins})  
+   await recordbooks({coins, mktparams}) 
    //await initDB()
    //text = await dbquery()
-    const response = {
+   const response = {
         statusCode: 200,
         body: text,
     }
